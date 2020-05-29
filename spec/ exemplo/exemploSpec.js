0bbc2016a28fe9e77ca3a0d8484b51de2a38bf26
Alcir dos Santos Monteiro <alcirmonteiro@gmail.com>
2020-05-29 19:07:10 -0300

    Arquivos do projeto local

---
+++ spec/ exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe("suite de testes de exemplo com NodeJs", () => {
+	it("Deve demosntrar o uso do jasmine com o NodeJs", () => {
+		expect(true).toEqual(true);
+	})
+
+	it("Deve demosntrar o uso do karma em modo de dev", () => {
+		expect(1).toEqual(1);
+	})
+})
